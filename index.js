33802eb2716ba790e13f83444c50387d15f429ba
Antoni Roman <antonipaulo.roman@gmail.com>
2026-03-18 20:38:22 -0500

    add javascript and link it

---
+++ index.js
@@ -0,0 +1,17 @@
+let code1 = 7 + 3;
+
+let code2 = 100 - 120/2;
+
+let code3 = 398 % 80;
+
+let message = "The vault has been secured. The combination is:";
+
+let codeA = code1 + "-" + code2 + "-" + code3;
+
+let codeB = `${code1}-${code2}-${code3}`;
+
+console.log(message);
+
+console.log(codeA);
+
+console.log(codeB);
